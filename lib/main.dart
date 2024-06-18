2573b095b8758158a73ab6b58710f44c6845c7d0
Yousef Montash <yousefmontash2002@gmail.com>
2024-06-18 16:11:52 +0300

    splash view body created

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashView(),
+    );
+  }
+}

2ab729ffe906078fbdb0fd42e8a608c2e1f0c099
Yousef Montash <yousefmontash2002@gmail.com>
2024-06-22 18:10:49 +0300

    creating home repo and failures abstract class

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failures,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failures,List<BookModel>>> fetchFeaturedBooks();
+}
